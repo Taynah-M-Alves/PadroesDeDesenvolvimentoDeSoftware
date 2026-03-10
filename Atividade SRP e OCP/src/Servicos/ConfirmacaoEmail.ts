d103ba239b457263b5fa1827066ee045aedbe2cd
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2026-03-09 20:27:10 -0400

    Renomeando arquivo

---
+++ Atividade SRP e OCP/src/Servicos/ConfirmacaoEmail.ts
@@ -0,0 +1,5 @@
+
+export class ConfirmacaoEmail{
+    enviarConfirmacao(email: string, valor: number): void {
+    console.log(`Enviando email para: ${email}: Seu pedido no valor de R$ ${valor} foi confirmado!`);}
+}
