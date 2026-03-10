bb477379459bee25894dca9274aa902a76fc9307
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2026-03-09 20:34:01 -0400

    criação da classe de ImpostoLivro

--- Atividade SRP e OCP/src/index.ts
+++ Atividade SRP e OCP/src/index.ts
@@ -5,6 +5,8 @@ import { ConfirmacaoEmail } from "./Servicos/ConfirmacaoEmail";
 
 import { ImpostoVestuario } from "./Imposto/ImpostoVestuario";
 import { PagamentoPix } from "./Pagamento/PagamentoPix";
+import { ImpostoLivro } from "./Imposto/ImpostoLivro";
+import { PagamentoCartao } from "./Pagamento/PagamentoCartao";
 
 const calculadora = new CalculadoraPedido(new ImpostoVestuario());
 const pagamento = new GeradorPagamento(new PagamentoPix());
@@ -20,4 +22,19 @@ pedido.processarPedido(
   100, 
   2,   
   "angela@gmail.com"
+);
+
+const calculadora2 = new CalculadoraPedido(new ImpostoLivro());
+const pagamento2 = new GeradorPagamento(new PagamentoCartao());
+
+const pedido2 = new PedidoService(
+  calculadora2,
+  pagamento2,
+  email
+);
+
+pedido2.processarPedido(
+  10, 
+  5,   
+  "taynah.micheli@gmail.com"
 );
